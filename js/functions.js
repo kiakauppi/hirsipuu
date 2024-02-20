9de20eeb4fb9f2cfb1a09a64626217845f9133d3
kiakauppi <n2kaki00@students.oamk.fi>
2024-02-20 14:49:45 +0200

    Tehtävä tehty loppuun

---
+++ js/functions.js
@@ -0,0 +1,70 @@
+const input = document.querySelector('input')
+const output = document.querySelector('output')
+const span = document.querySelector('span')
+
+const words = [
+    "programming",
+    "javascript",
+    "database",
+    "markup",
+    "framework",
+    "variable",
+    "stylesheet",
+    "library",
+    "asynchronous",
+    "hypertext"
+]
+
+let randomizedWord = ''
+let maskedWord = ''
+let numberOfGuesses = 0
+
+const newGame= () => {
+    const random = Math.floor(Math.random() * 10) + 1
+    randomizedWord = words [random]
+    maskedWord = "*".repeat(randomizedWord.length)
+    console.log(randomizedWord)
+    output.innerHTML = maskedWord
+
+}
+
+const win = () => {
+    alert(`Arvasit oikein, sana on ${randomizedWord}. Arvauskertoja oli yhteensä ${numberOfGuesses}.`)
+    newGame()
+    numberOfGuesses = 0
+}
+
+const replaceFoundChars = (guess) => {
+    for (let i = 0; i < randomizedWord.length; i++) {
+        const char = randomizedWord.substring(i,i+1)
+        if (char === guess) {
+            let newString = maskedWord.split('')
+            newString.splice(i,1,guess)
+            newString = newString.join('')
+            maskedWord = newString
+        }
+    }
+    output.innerHTML = maskedWord
+}
+
+newGame()
+
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter'){
+        e.preventDefault() //Prevent form submission.
+        numberOfGuesses= numberOfGuesses + 1
+        const guess = input.value
+        if (guess.toLowerCase() === randomizedWord.toLowerCase()) {
+            win()
+        } else if (guess.length === 1) {
+            replaceFoundChars(guess)
+            if (maskedWord.toLocaleLowerCase() === randomizedWord.toLocaleLowerCase()){
+                win()
+            }
+        } else {
+            alert("Arvasit väärin!")
+        }
+        input.value = ''
+    }
+    span.innerHTML = numberOfGuesses
+})
